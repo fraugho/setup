2d58fa4a209fa9d4ed3a850da3c958c03ef9cf45
fraugho <aghoanebom@gmail.com>
2025-02-08 21:25:51 -0900

    08/02/2025 21:25

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo chmod +x ~/code/setup/my.sh
+sudo ln -s ~/code/setup/my.sh /usr/bin/my
+
 dnf list | awk '{sub(/\.x86_64$/,"", $1); sub(/\.i686$/, "", $1); print $1}' >packages.txt
 
 chmod +x dnf.sh flatpak.sh git.sh
